3c85fd4154c050dca819fa130f58030235e975cd
jocelyn <jojoe45@hotmail.fr>
2019-03-25 09:55:38 +0100

    01 - Angular - Les Composants fix

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Signup } from '../sign-up';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  email = 'hello@gmail.com';
+
+    mail: Signup = {
+      email: 'hello@gmail.com'
+  }
 
   constructor() { }
 
